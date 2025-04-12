f4f21bc67b402d38cb53092901acad9f546da52b
Robin <127445060+robin160401@users.noreply.github.com>
2025-04-12 08:01:38 +0200

    fixed echo multiple env vars chained b ug

--- src/parsing/envs.c
+++ src/parsing/envs.c
@@ -53,8 +53,8 @@ static char	*replace_env_var_in_string(char *input, int i)
 	char	*output;
 
 	env_key_end = i;
-	while ((ft_isalnum(input[env_key_end])
-			|| ft_strchr("$_", input[env_key_end])) && input[env_key_end])
+	env_key_end++;
+	while ((ft_isalnum(input[env_key_end])) && input[env_key_end])
 		env_key_end++;
 	env_key = ft_substr(input, i + 1, env_key_end - i - 1);
 	env_val = ft_getenv(env_key);
